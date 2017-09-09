1a6da4812d445bacf3c93b6da98790e7f7f5dce1
Andrew Fischer <afischer15@mac.com>
2017-09-09 12:17:52 -0400

    super simple dater using wayback machine

--- scripts/dater.rb
+++ scripts/dater.rb
@@ -1,20 +1,45 @@
 require 'net/http'
 
+
+# [
+#  "urlkey",
+#  "timestamp",
+#  "original",
+#  "mimetype",
+#  "statuscode",
+#  "digest",
+#  "redirect",
+#  "robotflags",
+#  "length",
+#  "offset",
+#  "filename"
+# ],
+# Timestamp is in format yyyyMMddhhmmss
+
 class Dater
+  attr_accessor(:source)
   attr_accessor(:url)
 
   def initialize(url)
     @url = url
+    @source = "Wayback Machine"
   end
 
   def getArchiveURI(url)
     "http://web.archive.org/web/timemap/json/#{url}"
   end
 
-  def run
-    wayback_json = Net::HTTP.get_response(URI.parse(getArchiveURI(@url)))
-    data = wayback_json.body
-    JSON.parse(data).to_s
+  def getDateList
+    res = Net::HTTP.get_response(URI.parse(getArchiveURI(@url)))
+    body = res.body
+    json = JSON.parse(body)
+    json.shift # pop off header row
+    timestamps = json.map { |archive| Date.parse(archive[1]) }
+    timestamps
   end
 
+  def run
+    earliest = getDateList.min
+    "#{url} was first found on #{@source} on #{earliest}"
+  end
 end
