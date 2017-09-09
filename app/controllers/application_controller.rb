420582fe12a992a2e8a2c5825a50bf9fdde2e3a7
Alex Graves <graves@thedp.com>
2017-09-09 10:51:11 -0400

    add form for entering url to scrape

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 require 'sinatra'
+require 'uri'
+require './scripts/scraper.rb'
+
 class ApplicationController < Sinatra::Base
   use Rack::MethodOverride
   set :views, File.expand_path('../../views', __FILE__)
   set :public_folder, 'public'
 
   get '/' do
+    erb :'index.html'
+  end
+
+  post '/' do
+    uri = URI(params[:url])
+    url = uri.to_s
+    url = 'http://' + url unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+    scraper = Scraper.new(url)
+    scraper.run
     erb :'index.html'
   end
 end
--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -2,42 +2,52 @@ require 'nokogiri'
 require 'open-uri'
 require 'uri'
 
-@url = 'http://alex-graves.com'
-@main_page = Nokogiri::HTML(open(@url))
-@urls = [URI(@url)]
-@img_urls = []
+# creates a scraper object to scrape all images within the given website
+class Scraper
+  attr_accessor(:url)
 
-# convert all URLs to absolute URLs
-def convert_url(rel_url)
-  URI.join(@url, rel_url).to_s
-end
-
-# recurse on page if appropriate
-def scrape_links(link)
-  return if link.attr('href').nil?
-  url = convert_url(link.attr('href'))
-  return unless url.include?(@url)
-  return if @urls.include?(URI(url))
-  @urls << URI(url)
-  scrape(Nokogiri::HTML(open(url)))
-end
-
-# add image URL to array
-def scrape_images(img)
-  return if img.attr('src').nil?
-  img_url = convert_url(img.attr('src'))
-  @img_urls << img_url
-end
-
-# recursively scrape all links and images within given website
-def scrape(page)
-  page.search('a', 'img').each do |link|
-    scrape_images(link) if link.name == 'img'
-    scrape_links(link) if link.name == 'a'
+  def initialize(url)
+    @url = url
+    @main_page = Nokogiri::HTML(open(@url))
+    @urls = [URI(@url)]
+    @img_urls = []
   end
-end
 
-scrape(@main_page)
-@img_urls.each do |url|
-  p url
+  # convert all URLs to absolute URLs
+  def convert_url(rel_url)
+    URI.join(@url, rel_url).to_s
+  end
+
+  # recurse on page if appropriate
+  def scrape_links(link)
+    return if link.attr('href').nil?
+    url = convert_url(link.attr('href'))
+    return unless url.include?(@url)
+    return if @urls.include?(URI(url))
+    @urls << URI(url)
+    scrape(Nokogiri::HTML(open(url)))
+  end
+
+  # add image URL to array
+  def scrape_images(img)
+    return if img.attr('src').nil?
+    img_url = convert_url(img.attr('src'))
+    @img_urls << img_url
+
+    # printing currently just to show output
+    p img_url
+  end
+
+  # recursively scrape all links and images within given website
+  def scrape(page)
+    page.search('a', 'img').each do |link|
+      scrape_images(link) if link.name == 'img'
+      scrape_links(link) if link.name == 'a'
+    end
+  end
+
+  # actually runs the scraper
+  def run
+    scrape(@main_page)
+  end
 end
