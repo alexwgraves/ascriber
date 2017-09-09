e89a059b165d5c9886caca3e89a423631432b304
Andrew Fischer <afischer15@mac.com>
2017-09-09 00:36:05 -0400

    search for locations of images on other websites

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'google-cloud-vision', '~> 0.20.0'
+gem 'googleauth'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 require 'sinatra'
+require 'googleauth'
+
+ENV['GOOGLE_APPLICATION_CREDENTIALS'] = File.expand_path('../../throwaway-key.json', __FILE__)
+
 class ApplicationController < Sinatra::Base
   use Rack::MethodOverride
   set :views, File.expand_path('../../views', __FILE__)
@@ -7,4 +11,30 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :'index.html'
   end
+
+  get '/api' do
+    project_id = 'ascriber-179402'
+    image_path = 'https://i.imgur.com/X3VJ8Ax.jpg'
+
+    scopes = ['https://www.googleapis.com/auth/cloud-platform', 'https://www.googleapis.com/auth/cloud-vision']
+    authorization = Google::Auth.get_application_default(scopes)
+
+    require 'google/cloud/vision'
+
+    vision = Google::Cloud::Vision.new project: project_id
+    image = vision.image image_path
+
+    web = image.web
+
+    web.entities.each do |entity|
+      entity.description
+    end
+
+    web.full_matching_images.each do |image|
+      puts image
+    end
+
+    erb :'index.html'
+  end
+
 end
