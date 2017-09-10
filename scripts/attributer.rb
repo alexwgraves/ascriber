a5286133729e5b7e4d0e8bfa2ff0079edb007678
Alex Graves <graves@thedp.com>
2017-09-09 22:41:59 -0400

    results page copy changes

--- scripts/attributer.rb
+++ scripts/attributer.rb
@@ -3,12 +3,7 @@ class Attributer
     first = matches.first[:url]
     earliest = matches.reject { |m| m[:pubDate].nil? }.sort_by { |m| m[:pubDate] }
     probable_og = earliest.first
-    "<a href='#{first}' target='_blank'>Most Relevant URL</a><br>
-    Credit: #{scrape_credit}<br>
-    <a href='#{probable_og[:url]}' target='_blank'>Possible Earliest Source</a><br>
-    Credit: #{scrape_credit}<br>"
-  end
-
-  def self.scrape_credit
+    "<a href='#{first}' target='_blank'>Most Relevant URL</a> |
+    <a href='#{probable_og[:url]}' target='_blank'>Possible Earliest Source</a>"
   end
 end
