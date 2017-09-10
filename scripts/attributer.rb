d918dcd0d0b0491be34e76f28a6e1e76fc429645
Alex Graves <graves@thedp.com>
2017-09-10 03:11:17 -0400

    finishing up style and copy

--- scripts/attributer.rb
+++ scripts/attributer.rb
@@ -3,7 +3,7 @@ class Attributer
     first = matches.first[:url]
     earliest = matches.reject { |m| m[:pubDate].nil? }.sort_by { |m| m[:pubDate] }
     probable_og = earliest.first
-    "<a href='#{first}' target='_blank'>Most Relevant URL</a> |
+    "<a href='#{first}' target='_blank'>Most Relevant</a> |
     <a href='#{probable_og[:url]}' target='_blank'>Possible Earliest Source</a>"
   end
 end
